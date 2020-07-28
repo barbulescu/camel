2fffdff427f6f495c88d4b09c72c7ff3bbaf619c
Marius Barbulescu <marius.barbulescu@gmail.com>
2020-07-28 19:42:03 +0200

    first unit test

---
+++ src/main/java/com/barbulescu/camel/FolderCopyRouteBuilder.java
@@ -0,0 +1,14 @@
+package com.barbulescu.camel;
+
+import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FolderCopyRouteBuilder extends EndpointRouteBuilder {
+
+    @Override
+    public void configure() {
+        from(file("/tmp/camel_source").noop(true))
+                .to(file("/tmp/camel_destination"));
+    }
+}
--- src/test/java/com/barbulescu/camel/CamelApplicationTests.java
+++ src/test/java/com/barbulescu/camel/CamelApplicationTests.java
@@ -1,13 +1,50 @@
 package com.barbulescu.camel;
 
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
+import org.apache.camel.test.spring.junit5.CamelSpringTestSupport;
+import org.apache.camel.test.spring.junit5.MockEndpoints;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
-class CamelApplicationTests {
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
+
+@CamelSpringBootTest
+@ContextConfiguration
+@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
+@MockEndpoints
+class CamelApplicationTests extends CamelSpringTestSupport {
+
+    @Produce("mock://file:///tmp/camel_source")
+    private ProducerTemplate template;
+
+    @EndpointInject("mock://file:///tmp/camel_source")
+    private MockEndpoint source;
+    @EndpointInject("mock://file:///tmp/camel_destination")
+    private MockEndpoint destination;
 
     @Test
-    void contextLoads() {
+    public void testReceive() throws Exception {
+        assertThat(template).isNotNull();
+        source.expectedBodiesReceived("Hallo");
+        destination.expectedBodiesReceived("Hallo");
+
+        template.sendBody("Hallo");
+
+        MockEndpoint.assertIsSatisfied(5, SECONDS);
+        MockEndpoint.assertIsSatisfied(5, SECONDS);
     }
 
+    @Override
+    protected AbstractApplicationContext createApplicationContext() {
+        return new AnnotationConfigApplicationContext(FolderCopyRouteBuilder.class.getPackageName());
+    }
 }
